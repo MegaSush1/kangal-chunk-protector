1a492d8327d614dc78c169b6683b9473d7de5393
MegaSush1 <supersushispace@gmail.com>
2026-05-12 22:56:53 +0200

    Going from ChunkPos to Long for performance

--- src/main/java/kangal/chunk_protector/protection/ProtectedChunkManager.java
+++ src/main/java/kangal/chunk_protector/protection/ProtectedChunkManager.java
@@ -1,33 +1,28 @@
 package kangal.chunk_protector.protection;
 
+import it.unimi.dsi.fastutil.longs.LongOpenHashSet;
 import it.unimi.dsi.fastutil.longs.LongSet;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.Level;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import static kangal.chunk_protector.Kangal_chunk_protector.MOD_LOGGER;
-
 public class ProtectedChunkManager {
 
-    private static final Set<ChunkPos> PROTECTED_CHUNKS = new HashSet<>();
+    private static final LongSet PROTECTED_CHUNKS = new LongOpenHashSet();
 
     static {
-        addProtectedChunk(0, 0);
+        //addProtectedChunk(new ChunkPos(0, 0));
     }
 
-    public static void addProtectedChunk(int chunkX, int chunkZ){
-        PROTECTED_CHUNKS.add(new ChunkPos(chunkX,chunkZ));
+    public static void addProtectedChunk(Level world, ChunkPos chunkPos){
+
+        PROTECTED_CHUNKS.add( chunkPos.pack() );
     }
 
-    public static void removeProtectedChunk(int chunkX, int chunkZ){
-        PROTECTED_CHUNKS.remove(new ChunkPos(chunkX,chunkZ));
+    public static void removeProtectedChunk(Level world, ChunkPos chunkPos){
+        PROTECTED_CHUNKS.remove( chunkPos.pack() );
     }
 
     public static boolean isProtectedChunk(Level world, ChunkPos chunkPos){
-        MOD_LOGGER.info(String.format("Checking if chunk %s", chunkPos));
-        MOD_LOGGER.info(String.format("Chunk : %b", PROTECTED_CHUNKS.contains(chunkPos)));
-        return PROTECTED_CHUNKS.contains(chunkPos);
+        return PROTECTED_CHUNKS.contains(chunkPos.pack());
     }
 }
