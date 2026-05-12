3d9a5d009ae13ecfed4db2791c0c63afc32ed483
MegaSush1 <supersushispace@gmail.com>
2026-05-12 19:33:32 +0200

    Initial commit

---
+++ src/main/java/kangal/chunk_protector/Kangal_chunk_protector.java
@@ -0,0 +1,17 @@
+package kangal.chunk_protector;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Kangal_chunk_protector implements ModInitializer {
+
+	public static final String MOD_ID = "kangalchunkprotector";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
