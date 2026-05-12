e9342fe61cf5226e17cf6dce6b0401617fe4191e
MegaSush1 <supersushispace@gmail.com>
2026-05-12 23:16:16 +0200

    Multi dimension support

--- src/main/java/kangal/chunk_protector/protection/ProtectedChunkManager.java
+++ src/main/java/kangal/chunk_protector/protection/ProtectedChunkManager.java
@@ -2,27 +2,47 @@ package kangal.chunk_protector.protection;
 
 import it.unimi.dsi.fastutil.longs.LongOpenHashSet;
 import it.unimi.dsi.fastutil.longs.LongSet;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.Level;
 
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ProtectedChunkManager {
 
-    private static final LongSet PROTECTED_CHUNKS = new LongOpenHashSet();
-
-    static {
-        //addProtectedChunk(new ChunkPos(0, 0));
-    }
+    private static final Map<ResourceKey<Level>, LongSet> PROTECTED_CHUNKS = new HashMap<>();
 
     public static void addProtectedChunk(Level world, ChunkPos chunkPos){
+        long chunk = chunkPos.pack();
 
-        PROTECTED_CHUNKS.add( chunkPos.pack() );
+        PROTECTED_CHUNKS
+                .computeIfAbsent(
+                        world.dimension(),
+                        key -> new LongOpenHashSet()
+                )
+                .add(chunk);
     }
 
     public static void removeProtectedChunk(Level world, ChunkPos chunkPos){
-        PROTECTED_CHUNKS.remove( chunkPos.pack() );
+
+        long chunk = chunkPos.pack();
+
+        LongSet chunks = PROTECTED_CHUNKS.get(world.dimension());
+
+        if (chunks != null){
+            chunks.remove(chunk);
+        }
+
     }
 
     public static boolean isProtectedChunk(Level world, ChunkPos chunkPos){
-        return PROTECTED_CHUNKS.contains(chunkPos.pack());
+
+        LongSet chunks = PROTECTED_CHUNKS.get(world.dimension());
+
+        if(chunks == null) return false;
+
+        return chunks.contains(chunkPos.pack());
     }
 }
