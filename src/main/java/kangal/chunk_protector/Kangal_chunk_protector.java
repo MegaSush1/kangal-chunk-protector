a1cf76f8401f33de77828e19ce3e1bb8aa6ecc90
MegaSush1 <supersushispace@gmail.com>
2026-05-12 22:57:31 +0200

    Cleaning the onInitialize methods

--- src/main/java/kangal/chunk_protector/Kangal_chunk_protector.java
+++ src/main/java/kangal/chunk_protector/Kangal_chunk_protector.java
@@ -1,12 +1,8 @@
 package kangal.chunk_protector;
 
-import kangal.chunk_protector.protection.ProtectionUtils;
+import kangal.chunk_protector.protection.ProtectionEvents;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
-import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
-import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
-import net.fabricmc.fabric.api.event.player.UseBlockCallback;
-import net.minecraft.world.InteractionResult;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,37 +14,7 @@ public class Kangal_chunk_protector implements ModInitializer {
 	@Override
 	public void onInitialize() {
 
-		// BLOCK BREAK
-		PlayerBlockBreakEvents.BEFORE.register((level, player, pos, state, blockEntity)->{
-			MOD_LOGGER.info(MOD_ID + ": BlockBreakEvents.BEFORE");
-			return ProtectionUtils.canModify(player, pos);
-		});
+		ProtectionEvents.register();
 
-		// BLOCK PLACE
-		UseBlockCallback.EVENT.register((player, level, interactionHand, blockHitResult) ->{
-			MOD_LOGGER.info(MOD_ID + ": UseBlockCallback.EVENT");
-
-			if(!ProtectionUtils.canModify(player, blockHitResult.getBlockPos())) {
-				return InteractionResult.FAIL;
-			}
-
-			return InteractionResult.PASS;
-		});
-
-		// PLAYER ATTACK ENTITY
-		AttackEntityCallback.EVENT.register((player, level, interactionHand, entity, entityHitResult) -> {
-			MOD_LOGGER.info(MOD_ID + ": AttackEntityCallback.EVENT");
-
-			if (!ProtectionUtils.canAttack(player, entity)) {
-				return InteractionResult.FAIL;
-			}
-			return InteractionResult.PASS;
-		});
-
-		// ENTITY DAMAGE
-		ServerLivingEntityEvents.ALLOW_DAMAGE.register((livingEntity, damageSource, v) ->{
-			MOD_LOGGER.info(MOD_ID + ": ServerLivingEntityEvents.ALLOW_DAMAGE");
-			return ProtectionUtils.canDamage(livingEntity, damageSource);
-		});
 	}
 }
